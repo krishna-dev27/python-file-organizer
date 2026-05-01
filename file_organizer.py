93ab8c545b0d24bbe11ea75a1a47bacaf093da2b
krishna-dev27 <krishnaegv@gmail.com>
2026-05-01 12:47:00 +0530

    optimized and exception handled

--- file_organizer.py
+++ file_organizer.py
@@ -1,58 +1,42 @@
 
 
-
 import os
 import shutil
+folder_path = input()
 
-# 1. Folder path to organize
-
-folder_path = input("Enter full path that you want to organize:")
-
-# 2. File type categories
-file_types={
-    'Images':['.jpg','.png','.jpeg','.bmp','.gif','.webp','.svg'],
-    'Documents':['.pdf','.txt','.xml','.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.csv', '.md'],
-    'Music':['.mp3','.wav', '.flac', '.aac', '.ogg', '.m4a'],
-    'Videos':['.mp4','.mkv', '.avi', '.mov', '.wmv', '.webm']
-
+file_formats = {
+    'images': ['.jpeg', '.jpg', '.png'],
+    'documents': ['.pdf', '.doc', '.docx'],
+    'videos': ['.mp4']
 }
 
-# 3. Get all items inside the folder
-files = os.listdir(folder_path)
-
-# 4. Loop through each item
-for file in files:
-    file_path = os.path.join(folder_path, file)
-
-    # Process only files (not folders)
-    if os.path.isfile(file_path):
-        name, extension = os.path.splitext(file)
-        moved = False
-
-        # Check file type
-        for folder_name, extensions in file_types.items():
-            if extension.lower() in extensions:
-                destination_folder = os.path.join(folder_path, folder_name)
-
-                # Create folder if it doesn't exist
-                if not os.path.exists(destination_folder):
-                    os.mkdir(destination_folder)
-
-                # Move file
-                shutil.move(file_path, destination_folder)
-                print(file, "moved to", folder_name)
-                moved = True
-                break
-
-        # If file type not matched, move to Others
-        if not moved:
-            other_folder = os.path.join(folder_path, "Others")
-
-            if not os.path.exists(other_folder):
-                os.mkdir(other_folder)
-
-            shutil.move(file_path, other_folder)
-            print(file, "moved to Others")
+ext_map={}
+for folder, extension in file_formats.items():
+    for ext in extension:
+        ext_map[ext]=folder
 
 
-print("Hoooray!!! Your folder is now arranged😊")
+for file in os.listdir(folder_path):
+    file_path=os.path.join(folder_path,file)
+    if not os.path.isfile(file_path):
+        continue
+    file_name, file_extension = os.path.splitext(file)
+    file_extension = file_extension.lower()
+     
+    target_folder = ext_map.get(file_extension,'others')
+    target_path = os.path.join(folder_path,target_folder)
+    os.makedirs(target_path,exist_ok=True)
+    destination = os.path.join(target_path,file)
+
+
+    counter = 1
+    base_name ,extension = os.path.splitext(file)
+    while os.path.exists(destination):
+        new_name = f"{base_name}_{counter}{extension}"
+        destination = os.path.join(target_path, new_name)
+        counter += 1
+    try:
+        shutil.move(file_path,destination)
+    except Exception as e:
+        print(f"Error moving {file}:{e}")
+
